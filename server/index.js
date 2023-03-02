6ec276e7af23909efd2f93d841b720d36115600e
Tyler <schultty@mail.uc.edu>
2023-03-02 18:26:09 -0500

    server start

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const app = require('./app');
+const http = require('http');
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+    });
